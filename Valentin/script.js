9309f5eab6a1163fbe548bc6ab407b5bb87afc2c
Kiwitann <axelvalle2222@gmail.com>
2024-02-11 00:08:01 -0600

    Creacion de archivos

---
+++ Valentin/script.js
@@ -0,0 +1,9 @@
+function funcionBoton1() {
+    // Lógica para el botón 1
+    alert("Botón 1 clickeado");
+}
+
+function funcionBoton2() {
+    // Lógica para el botón 2
+    alert("Botón 2 clickeado");
+}
